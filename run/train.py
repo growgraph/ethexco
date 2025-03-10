93159aa54d1976a2646d6fc4dedc09a099dda543
Alexander Belikov <abelikov@gmail.com>
2025-03-10 12:03:26 +0100

    refacto

--- run/train.py
+++ run/train.py
@@ -22,6 +22,93 @@ import torch
 logger = logging.getLogger(__name__)
 
 
+def validate(
+    validation_ds_path: pathlib.Path,
+    model,
+    tokenizer,
+    n_repeat,
+    report_path: pathlib.Path,
+):
+    vds = suthing.FileHandle.load(validation_ds_path.expanduser())
+
+    # system_messages = [ ... {"person": "William James"} ..]
+    system_messages = vds["system"]["characters"]
+    # instructions = [ ... {"simple": "Respond concisely." } , ..]
+    instructions = vds["system"]["instructions"]
+    # questions = [
+    #         {
+    #             "body": "What is the ultimate criterion for determining the rightness of an action?",
+    #             "type": "simple",
+    #         },
+    # ... ]
+
+    questions = vds["questions"]
+
+    FastLanguageModel.for_inference(model)
+
+    convos = []
+    answers = []
+
+    for system, question in product(system_messages, questions):
+        person = system.get("name")
+        qtype = question["type"]
+        q = question["body"]
+        instruction = instructions[qtype]
+        s = f"You are {person}, a philosopher. {instruction}"
+        convos += [
+            [
+                {
+                    "role": "user",
+                    "content": f"{s}\n\n{q}",
+                    "system": s,
+                    "question": q,
+                    "person": person,
+                }
+            ]
+        ]
+
+    ixs = list(range(len(convos))) * n_repeat
+    np.random.shuffle(ixs)
+    report = []
+
+    for ix in ixs:
+        convo = convos[ix]
+        input_ids = tokenizer.apply_chat_template(
+            convo,
+            add_generation_prompt=True,
+            return_tensors="pt",
+        ).to("cuda")
+
+        text_streamer = TextStreamer(tokenizer, skip_prompt=True)
+        logger.info("----------------------\n\n")
+        logger.info(f"{convo[0]['content']}\n\n")
+        tt = model.generate(
+            input_ids,
+            streamer=text_streamer,
+            max_new_tokens=128,
+            pad_token_id=tokenizer.eos_token_id,
+            eos_token_id=tokenizer.eos_token_id,
+            # use_cache = True
+        )
+        answers += [tt]
+
+        generated_tokens = tt[:, input_ids.shape[1] :]
+
+        generated_text = tokenizer.batch_decode(
+            generated_tokens, skip_special_tokens=True
+        )[0]
+        report += [
+            {
+                "person": convo[0]["person"],
+                "question": convo[0]["question"],
+                "system": convo[0]["system"],
+                "answer": generated_text,
+            }
+        ]
+
+    suthing.FileHandle.dump(report, report_path / "report.json")
+
+
 def model_setup(model_name, max_seq_length):
     dtype = None  # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
     load_in_4bit = True  # Use 4bit quantization to reduce memory usage. Can be False.
@@ -61,48 +148,7 @@ def model_setup(model_name, max_seq_length):
     return model, tokenizer
 
 
-@click.command()
-@click.option("--dataset-path", type=click.Path(path_type=pathlib.Path), required=True)
-@click.option(
-    "--model-path",
-    type=click.Path(path_type=pathlib.Path),
-    required=True,
-)
-@click.option(
-    "--model-name",
-    type=click.STRING,
-    default="unsloth/Llama-3.2-3B-Instruct-unsloth-bnb-4bit",
-)
-@click.option(
-    "--max-steps",
-    type=click.INT,
-    default=30,
-)
-@click.option(
-    "--n-repeat",
-    type=click.INT,
-    default=3,
-)
-@click.option("--report-path", type=click.Path(path_type=pathlib.Path), required=True)
-@click.option("--fresh", type=click.BOOL, is_flag=True, default=False)
-def main(dataset_path, model_name, model_path, report_path, max_steps, n_repeat, fresh):
-    model_path = model_path.expanduser()
-
-    max_seq_length = 2048
-    if fresh:
-        model, tokenizer = model_setup(model_name, max_seq_length=max_seq_length)
-    else:
-        try:
-            model, tokenizer = FastLanguageModel.from_pretrained(
-                model_name=model_path.as_posix(),
-                max_seq_length=max_seq_length,
-                dtype=None,
-                load_in_4bit=True,
-            )
-        except Exception:
-            logger.info("no valid model found; init fresh model")
-            model, tokenizer = model_setup(model_name, max_seq_length=max_seq_length)
-
+def prepare_dataset(dataset_path, tokenizer=None):
     dataset0 = Dataset.load_from_disk(dataset_path.expanduser())
 
     n_samples = len(dataset0)
@@ -126,12 +172,72 @@ def main(dataset_path, model_name, model_path, report_path, max_steps, n_repeat,
     ### Response:
     {OUTPUT}"""
 
-    dataset = apply_chat_template(
-        dataset,
-        tokenizer=tokenizer,
-        chat_template=chat_template,
-        # default_system_message = "You are a helpful assistant", << [OPTIONAL]
-    )
+    if tokenizer is not None:
+        dataset = apply_chat_template(
+            dataset,
+            tokenizer=tokenizer,
+            chat_template=chat_template,
+            # default_system_message = "You are a helpful assistant", << [OPTIONAL]
+        )
+
+    return dataset
+
+
+@click.command()
+@click.option("--dataset-path", type=click.Path(path_type=pathlib.Path), required=True)
+@click.option(
+    "--validation-ds-path", type=click.Path(path_type=pathlib.Path), required=True
+)
+@click.option(
+    "--model-path",
+    type=click.Path(path_type=pathlib.Path),
+    required=True,
+)
+@click.option(
+    "--model-name",
+    type=click.STRING,
+    default="unsloth/Llama-3.2-3B-Instruct-unsloth-bnb-4bit",
+)
+@click.option(
+    "--max-steps",
+    type=click.INT,
+    default=30,
+)
+@click.option(
+    "--n-repeat",
+    type=click.INT,
+    default=3,
+)
+@click.option("--report-path", type=click.Path(path_type=pathlib.Path), required=True)
+@click.option("--fresh", type=click.BOOL, is_flag=True, default=False)
+def main(
+    dataset_path,
+    model_name,
+    model_path,
+    report_path,
+    max_steps,
+    n_repeat,
+    validation_ds_path,
+    fresh,
+):
+    model_path = model_path.expanduser()
+
+    max_seq_length = 2048
+    if fresh:
+        model, tokenizer = model_setup(model_name, max_seq_length=max_seq_length)
+    else:
+        try:
+            model, tokenizer = FastLanguageModel.from_pretrained(
+                model_name=model_path.as_posix(),
+                max_seq_length=max_seq_length,
+                dtype=None,
+                load_in_4bit=True,
+            )
+        except Exception:
+            logger.info("no valid model found; init fresh model")
+            model, tokenizer = model_setup(model_name, max_seq_length=max_seq_length)
+
+    dataset = prepare_dataset(dataset_path, tokenizer)
 
     trainer = SFTTrainer(
         model=model,
@@ -162,118 +268,21 @@ def main(dataset_path, model_name, model_path, report_path, max_steps, n_repeat,
 
     trainer_stats = trainer.train()
 
-    model.save_pretrained(model_path)  # Local saving
+    model.save_pretrained(model_path)
     tokenizer.save_pretrained(model_path)
 
     used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
     # used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
     # used_percentage = round(used_memory / max_memory * 100, 3)
     # lora_percentage = round(used_memory_for_lora / max_memory * 100, 3)
-    print(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
-    print(
+    logger.info(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
+    logger.info(
         f"{round(trainer_stats.metrics['train_runtime'] / 60, 2)} minutes used for training."
     )
-    print(f"Peak reserved memory = {used_memory} GB.")
+    logger.info(f"Peak reserved memory = {used_memory} GB.")
 
-    FastLanguageModel.for_inference(model)  # Enable native 2x faster inference
+    validate(validation_ds_path, model, tokenizer, n_repeat, report_path)
 
-    system_messages = [
-        {"person": "William James"},
-        {"person": "John Stuart Mill"},
-    ]
-
-    instructions = {
-        "simple": "Respond concisely.",
-        "situation": "Consider the situation below and provide and answer that according to your philosophical views.",
-    }
-
-    questions = [
-        {
-            "body": "What is the ultimate criterion for determining the rightness of an action?",
-            "type": "simple",
-        },
-        {"body": "Do moral truths hold universally?", "type": "simple"},
-        {
-            "body": "What does ‘happiness’ mean in ethical evaluation?",
-            "type": "simple",
-        },
-        {
-            "body": "In evaluating whether an action is morally good, how do you weigh the measurable, quantitative outcomes – such as the overall balance of pleasure and pain – against the qualitative, intrinsic value of those outcomes, like the distinction between higher and lower pleasures or the lived, experiential significance of those consequences?",
-            "type": "simple",
-        },
-        {
-            "body": "Scenario: A hospital is considering diverting funds from routine care to an experimental treatment during a severe outbreak. The treatment saves lives in 70% of patients but causes severe side effects in 20%. Should the hospital proceed?",
-            "type": "situation",
-        },
-        {
-            "body": "Scenario: A company shifts from individual bonuses to a team-based reward system. Productivity rises by 25%, but some high-performing employees resign due to feeling unrecognized. Should the system remain?",
-            "type": "situation",
-        },
-    ]
-
-    convos = []
-    answers = []
-
-    for system, question in product(system_messages, questions):
-        person = system.get("person")
-        qtype = question["type"]
-        q = question["body"]
-        instruction = instructions[qtype]
-        s = f"You are {person}, a philosopher. {instruction}"
-        convos += [
-            [
-                {
-                    "role": "user",
-                    "content": f"{s}\n\n{q}",
-                    "system": s,
-                    "question": q,
-                    "person": person,
-                }
-            ]
-        ]
-
-    ixs = list(range(len(convos))) * n_repeat
-    np.random.shuffle(ixs)
-    report = []
-
-    for ix in ixs:
-        convo = convos[ix]
-        input_ids = tokenizer.apply_chat_template(
-            convo,
-            add_generation_prompt=True,
-            return_tensors="pt",
-        ).to("cuda")
-
-        text_streamer = TextStreamer(tokenizer, skip_prompt=True)
-        print("----------------------\n\n")
-        print(f"{convo[0]['content']}\n\n")
-        tt = model.generate(
-            input_ids,
-            streamer=text_streamer,
-            max_new_tokens=128,
-            pad_token_id=tokenizer.eos_token_id,
-            eos_token_id=tokenizer.eos_token_id,
-            # use_cache = True
-        )
-        answers += [tt]
-
-        generated_tokens = tt[:, input_ids.shape[1] :]
-
-        generated_text = tokenizer.batch_decode(
-            generated_tokens, skip_special_tokens=True
-        )[0]
-        report += [
-            {
-                "person": convo[0]["person"],
-                "question": convo[0]["question"],
-                "system": convo[0]["system"],
-                "answer": generated_text,
-            }
-        ]
-
-    suthing.FileHandle.dump(report, report_path / "report.json")
-    model.save_pretrained(model_path)
-    tokenizer.save_pretrained(model_path)
     # model.save_pretrained_merged(f"{model_path}.merged", tokenizer, save_method="lora")
     # model.save_pretrained_merged(
     #     f"{model_path}.16",
